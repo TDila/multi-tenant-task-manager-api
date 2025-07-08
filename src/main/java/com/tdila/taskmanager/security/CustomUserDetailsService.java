30f86739d6c7a248a55d85565ecc8ec8685fe847
Tharusha Dilhara <119704521+TDila@users.noreply.github.com>
2025-07-08 20:54:44 +0530

    Implement JWT-based authentication and security configuration
    - Added CustomUserDetailsService to load user info from DB
    
    - Created JwtAuthenticationFilter to validate JWT on each request
    
    - Added utility methods in JwtUtil (extractUsername, isTokenValid, isTokenExpired)
    
    - Configured Spring Security with SecurityConfig (stateless auth, role-based access)

---
+++ src/main/java/com/tdila/taskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.tdila.taskmanager.security;
+
+import com.tdila.taskmanager.entity.User;
+import com.tdila.taskmanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: "+email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                AuthorityUtils.createAuthorityList(user.getRole() != null ? user.getRole().name() : "NONE")
+        );
+    }
+}
--- src/main/java/com/tdila/taskmanager/util/JwtUtil.java
+++ src/main/java/com/tdila/taskmanager/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.tdila.taskmanager.util;
 import com.tdila.taskmanager.entity.User;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 
@@ -26,5 +27,28 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        final Date expiration = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+
+        return expiration.before(new Date());
     }
 }
